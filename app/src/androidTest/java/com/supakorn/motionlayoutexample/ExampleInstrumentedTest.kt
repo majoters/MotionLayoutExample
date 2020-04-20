55bddc56e26dd7241b778b4ec9099f919d94b745
Supakorn Komolhatai <supakorn.kom@truedigital.com>
2020-04-20 11:45:43 +0700

    Fix App name.

--- app/src/androidTest/java/com/supakorn/motionlayoutexample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/supakorn/motionlayoutexample/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.supakorn.motionlayoutexample2", appContext.packageName)
+        assertEquals("com.supakorn.motionlayoutexample", appContext.packageName)
     }
 }
